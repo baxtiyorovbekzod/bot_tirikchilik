9f3f3bb3894178adcd4ac5533ef7aff963dd0e2a
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-11-09 21:05:24 +0500

    ok

---
+++ bot.py
@@ -0,0 +1,56 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from config import Config
+from callbacks import start, send_orders, send_about, change_language, send_settings
+
+
+def main() -> None:
+    updater = Updater(Config.TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(
+        handler=CommandHandler(
+            command='start',
+            callback=start
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('📦 Buyurtmalarim'),
+            callback=send_orders
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('ℹ️ Biz haqimizda'),
+            callback=send_about
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('⚙️ Sozlamalar'),
+            callback=send_settings
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('Orqaga'),
+            callback=start
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('Tilni o\'zgartirish'),
+            callback=change_language
+        )
+    )
+
+    updater.start_polling()
+    updater.idle()
+
+main()
